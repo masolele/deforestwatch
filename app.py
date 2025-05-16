db7165f47a96641907b55c9520a3276e33a33860
Robert Masolele <31400779+masolele@users.noreply.github.com>
2025-05-16 11:52:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -68,10 +68,37 @@ if roi:
             pred_classes = np.argmax(pred, axis=-1).astype(np.uint8)
 
             # Color map
-            color_map = {
-                0: (153, 153, 153), 1: (255, 215, 0), 2: (34, 139, 34),
-                3: (31, 120, 180), 4: (176, 139, 109), 5: (227, 168, 87), 6: (204, 204, 204)
-            }
+            # color_map = {
+            #    0: (153, 153, 153), 1: (255, 215, 0), 2: (34, 139, 34),
+            #    3: (31, 120, 180), 4: (176, 139, 109), 5: (227, 168, 87), 6: (204, 204, 204)
+            #}
+
+            color_map = {1: (255, 235, 190),    # Large-scale cropland
+                         2: (227, 168, 87),     # Pasture
+                         3: (150, 75, 0),       # Mining
+                         4: (255, 204, 153),    # Small-scale cropland
+                         5: (128, 128, 128),    # Roads
+                         6: (140, 198, 63),     # Other-land with tree cover
+                         7: (0, 100, 0),        # Plantation forest
+                         8: (111, 64, 152),     # Coffee
+                         9: (153, 153, 153),    # Built-up
+                         10: (31, 120, 180),     # Water
+                         11: (255, 102, 102),   # Oil palm
+                         12: (5, 136, 179),     # Rubber
+                         13: (128, 64, 0),      # Cacao
+                         14: (204, 255, 204),   # Avocado
+                         15: (255, 153, 51),    # Soy
+                         16: (255, 255, 102),   # Sugar
+                         17: (255, 229, 204),   # Maize
+                         18: (255, 255, 153),   # Banana
+                         19: (255, 218, 185),   # Pineapple
+                         20: (230, 230, 250),   # Rice
+                         21: (160, 82, 45),     # Logging
+                         22: (255, 204, 229),   # Cashew
+                         23: (186, 85, 211),    # Tea
+                         24: (102, 153, 153),   # Others
+                         #24: (192, 192, 192),   # Unknown / fallback
+                        }
 
             rgb_image = np.zeros((*pred_classes.shape, 3), dtype=np.uint8)
             for cls, rgb in color_map.items():
