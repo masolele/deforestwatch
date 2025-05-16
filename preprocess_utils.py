4d1f497dc1c9bce31044083de557e6f84c73a8f1
Robert Masolele <31400779+masolele@users.noreply.github.com>
2025-05-16 13:24:58 +0200

    Update preprocess_utils.py

--- preprocess_utils.py
+++ preprocess_utils.py
@@ -1,1 +1,7 @@
+import numpy as np
 
+def preprocess_planet(roi):
+    # For now, return dummy array. Replace with actual preprocessing.
+    H, W = 256, 256
+    dummy = np.random.rand(H, W, 17).astype(np.float32)
+    return dummy
