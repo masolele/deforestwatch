2d127aa9296f8e6e281a5e02c250ee9185322101
Robert Masolele <31400779+masolele@users.noreply.github.com>
2025-06-10 18:51:18 +0200

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -49,13 +49,42 @@ def stack_channels2(channels):
 def stack_channels3(channels):
     return tf.stack([channels[14], channels[15], channels[16]], axis=-1)
 
+def attention_block(x, gating, inter_shape):
+    shape_x = K.int_shape(x)
+    shape_g = K.int_shape(gating)
+
+# Getting the x signal to the same shape as the gating signal
+    theta_x = layers.Conv2D(inter_shape, (2, 2), strides=(2, 2), padding='same')(x)  # 16
+    shape_theta_x = K.int_shape(theta_x)
+
+# Getting the gating signal to the same number of filters as the inter_shape
+    phi_g = layers.Conv2D(inter_shape, (1, 1), padding='same')(gating)
+    upsample_g = layers.Conv2DTranspose(inter_shape, (3, 3),
+                                 strides=(shape_theta_x[1] // shape_g[1], shape_theta_x[2] // shape_g[2]),
+                                 padding='same')(phi_g)  # 16
+
+    concat_xg = layers.add([upsample_g, theta_x])
+    act_xg = layers.Activation('relu')(concat_xg)
+    psi = layers.Conv2D(1, (1, 1), padding='same')(act_xg)
+    sigmoid_xg = layers.Activation('sigmoid')(psi)
+    shape_sigmoid = K.int_shape(sigmoid_xg)
+    upsample_psi = layers.UpSampling2D(size=(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2]))(sigmoid_xg)  # 32
+
+    upsample_psi = repeat_elem(upsample_psi, shape_x[3])
+
+    y = layers.multiply([upsample_psi, x])
+
+    result = layers.Conv2D(shape_x[3], (1, 1), padding='same')(y)
+    result_bn = layers.BatchNormalization()(result)
+    return result_bn
+    
 custom_objects = {
     'unstack_channels': unstack_channels,
     'stack_channels1': stack_channels1,
     'stack_channels2': stack_channels2,
     'stack_channels3': stack_channels3,
     # Include any other custom functions used in your model
-    'gating_signal': gating_signal,
+    #'gating_signal': gating_signal,
     'attention_block': attention_block
 }
 
@@ -83,7 +112,6 @@ def load_region_model(region_name):
         cache_dir="models"  # Store locally to avoid repeated downloads
     )
     #return load_model(model_path, compile=False)
-    return load_model(model_path, compile=False, custom_objects=custom_objects.update({
-    'Attention_UNetFusion3I_Sentinel': Attention_UNetFusion3I_Sentinel,custom_objects}))
+    return load_model(model_path, compile=False, custom_objects=custom_objects)
     #return load_model(model_path, compile=False, custom_objects={'PositionEmbedding': PositionEmbedding})
     
