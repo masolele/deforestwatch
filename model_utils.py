0664c40a9e1249c9feac064c3a10056697a98c77
Robert Masolele <31400779+masolele@users.noreply.github.com>
2025-06-10 18:33:05 +0200

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -60,6 +60,7 @@ def load_region_model(region_name):
         cache_dir="models"  # Store locally to avoid repeated downloads
     )
     #return load_model(model_path, compile=False)
-    return load_model(model_path, compile=False, custom_objects=custom_objects)
+    return load_model(model_path, compile=False, custom_objects=custom_objects.update({
+    'RES_Attention_UNetFusion3I_Sentinel': RES_Attention_UNetFusion3I_Sentinel}))
     #return load_model(model_path, compile=False, custom_objects={'PositionEmbedding': PositionEmbedding})
     
